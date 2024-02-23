7d22a0df53ed84e2ae38159b2448960b81dd9182
Jonathan Lifflander <jliffla@sandia.gov>
2024-02-22 16:07:24 -0800

    start writing logic engine

---
+++ LogicEngine.gd
@@ -0,0 +1,52 @@
+class_name LogicEngine extends Node
+
+@onready var tile_map : IsoTileMap = get_node("Tiles")
+
+var turn_counter : int = 0
+var players : Array[Player]
+
+class Player:
+	var player_id : int = -1
+	var last_turn_completed : int = -1
+	var units : Array[Unit]
+	var tile_map : IsoTileMap
+	
+	func _init(in_tile_map : IsoTileMap, in_player_id : int):
+		tile_map = in_tile_map
+		player_id = in_player_id
+		
+		# Dummy insertion of a unit
+		units.append(Unit.new(tile_map, Vector2i(player_id, player_id)))
+
+class UnitAbilities:
+	var distance : int = 1
+	var attack : int = 1
+	var hp : int = 10
+	var defense : int = 1
+
+class Unit:
+	var location : Vector2i
+	var turn_completed : bool = false
+	var selected : bool = false
+	var abilities : UnitAbilities = UnitAbilities.new()
+	var tile_map : IsoTileMap
+	
+	func _init(in_tile_map : IsoTileMap, in_location : Vector2i):
+		tile_map = in_tile_map
+		location = in_location
+		
+	func renderAtLocation():
+		tile_map.unit_layer[tile_map.convertTo1D(location)] = Vector2i(0, 0)
+
+func initialize(num_players : int):
+	for i in num_players:
+		players.append(Player.new(tile_map, i))
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Logic Engine is ready")
+	initialize(2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- TileMap.gd
+++ TileMap.gd
@@ -1,21 +1,44 @@
 extends TileMap
 class_name IsoTileMap
 
+const unit_tile_set : int = 1
+var width : int = 10
+var height : int = 10
 var special_cell : Vector2i = Vector2i(-1,-1)
+
+var unit_layer : Array[Vector2i]
+
+func convertTo1D(idx : Vector2i) -> int:
+	return idx.x * width + idx.y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Populate layers
+	# At the beginning no units exist to set all to -1
+	unit_layer.resize(width*height)
+	for x in range(width):
+		for y in range(height):
+			#print("x=", x, ", y=", y, ", 1d=", convertTo1D(Vector2i(x,y)))
+			unit_layer[convertTo1D(Vector2i(x,y))] = Vector2i(-1, -1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for x in range(10):
-		for y in range(10):
-			if special_cell.x == x and special_cell.y == y:
-				set_cell(0, Vector2i(x,y), 0, Vector2i(3,4), 0)
-			else:
-				set_cell(0, Vector2i(x,y), 0, Vector2i(x%4,y%4), 0)
+	var layer : int = 0
 
-	
-	#set_cell(0,Vector2i())
+	# Draw the base map layer
+	for x in range(width):
+		for y in range(height):
+			if special_cell.x == x and special_cell.y == y:
+				set_cell(layer, Vector2i(x,y), 0, Vector2i(3,4), 0)
+			else:
+				set_cell(layer, Vector2i(x,y), 0, Vector2i(x%4,y%4), 0)
+
+	# Draw the unit layer
+	layer = 1
+	for x in range(width):
+		for y in range(height):
+			var unit_to_draw : Vector2i = unit_layer[convertTo1D(Vector2i(x,y))]
+			if unit_to_draw.x != -1 and unit_to_draw.y != -1:
+				set_cell(layer, Vector2i(x,y), unit_tile_set, unit_to_draw, 0)
+
 
